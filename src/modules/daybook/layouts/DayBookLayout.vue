39807a66ebbbf5de98126285ffc69d047c885b25
hlanaumann@gmail.com <hlanaumann@gmail.com>
2022-07-19 12:13:49 +0200

    added the layout and some modules in daybook

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>DayBook Layout</h1>
+    <NavBarDay/>
+
+    <div class="d-flex">
+        <div class="col-4">
+            <EntryList/>
+        </div>
+        <div class="col">
+            <router-view/>
+        </div>
+    </div>
 </template>
+
+<script>
+
+import { defineAsyncComponent } from "vue";
+
+export default {
+    components: {
+        NavBarDay: defineAsyncComponent(()=> import('../components/NavBarDay.vue')),
+        EntryList: defineAsyncComponent(()=> import('../components/EntryList.vue')),
+    }
+}
+
+</script>
