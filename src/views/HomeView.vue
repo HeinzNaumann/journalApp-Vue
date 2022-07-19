8d15f57e8cdd0cafc5f8352b9812ef7b0b8bf33c
hlanaumann@gmail.com <hlanaumann@gmail.com>
2022-07-19 10:16:15 +0200

    created the modular structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Hola Mundo</h1>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-success">Success</button>
+    <button @click="gotToDayBook" class="btn btn-primary">Primary</button>
+    <button @click="gotToDayBook" class="btn btn-secondary">Secondary</button>
+    <button @click="gotToDayBook" class="btn btn-success">Success</button>
   </div>
 </template>
+
+
+<script>
+
+  export default {
+    methods: {
+      gotToDayBook(){
+        this.$router.push({name: 'daybook'})
+      }
+    }
+  }
+
+
+</script>
